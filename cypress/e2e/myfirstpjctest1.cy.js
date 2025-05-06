640151d2c9cd2c23d374845b9a72f1f64f788940
anbudeen <anbu.devai87@gmail.com>
2025-05-06 15:49:10 +0530

    added new projct

---
+++ cypress/e2e/myfirstpjctest1.cy.js
@@ -0,0 +1,6 @@
+/// <reference types='Cypress' />
+describe("my test suite project11",()=>{
+    it("my test cases of project1",()=>{
+        cy.visit("https://www.automationexercise.com/")
+    })
+})
